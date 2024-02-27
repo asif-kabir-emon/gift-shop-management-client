1a9e6167410fb4ccc8b54f4f38a23bcb37d9143d
MD ASIF KABIR EMON <79392749+asif-kabir-emon@users.noreply.github.com>
2024-02-28 00:59:57 +0800

    Update

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,12 @@
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    return (
+        <div className="flex flex-col gap-3">
+            <div className="bg-white text-[20px] font-bold text-center py-3">
+                <h1>Dashboard</h1>
+            </div>
+            <div className="bg-white text-[20px] font-bold text-center py-3 min-h-40"></div>
+        </div>
+    );
 };
 
 export default Dashboard;
